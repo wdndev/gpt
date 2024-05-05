8ed2f0c9d750e73dcebbe934e6d00331341b3369
wdndev <dongnian.wang@outlook.com>
2024-05-05 13:51:17 +0800

    generation

--- 360zhinao/models/generation_utils.py
+++ 360zhinao/models/generation_utils.py
@@ -12,8 +12,9 @@ def make_context(model, tokenizer,
                  system: str = "You are a helpful assistant.",
                  max_new_tokens: int=0, 
                 ):
-    
+    # 确定新生成的token数量，优先使用传入参数，否则使用模型配置中的默认值
     max_new_tokens = max_new_tokens or model.generation_config.max_new_tokens
+    # 计算模型允许的最大输入长度（模型最大长度减去新生成的token数）
     max_input_length = model.config.model_max_length - max_new_tokens
 
     im_start_id = [tokenizer.im_start_id]
@@ -21,15 +22,20 @@ def make_context(model, tokenizer,
     nl_tokens = tokenizer.encode("\n")
 
     def _tokenize_str(role, content):
+        """ 定义辅助函数，将角色和内容转化为token序列
+        """
         return tokenizer.encode(role, allowed_special=set()) + nl_tokens + tokenizer.encode(content, allowed_special=set())
     
     def _parse_messages(messages):
+        """ 解析消息列表，分离系统消息、查询和对话历史
+        """
         system, query, history = "", "", []
         ## system
         if messages[0]["role"] == "system":
             system = messages[0]["content"]
             messages = messages[1:]
         ## query
+        ### 确保最后一项是用户消息
         assert messages[-1]["role"] == "user"
         query = messages[-1]["content"]
         messages = messages[:-1]
@@ -41,6 +47,7 @@ def make_context(model, tokenizer,
 
         return system, query, history
     
+    # 调用_parse_messages解析消息
     _system, query, history = _parse_messages(messages)
 
     ## system
@@ -58,6 +65,7 @@ def make_context(model, tokenizer,
     max_history_length = max_input_length - len(system_tokens) - len(query_tokens) - len(final_tokens)
     
     ## history
+    ## 逆序遍历对话历史，构建token序列
     context_tokens = []
     for turn_query, turn_response in reversed(history):
         ## query tokens
@@ -67,6 +75,7 @@ def make_context(model, tokenizer,
         ## this round tokens
         next_context_tokens = history_query_tokens + histroy_response_tokens
         ## concat
+        ## 确保加入这些token后总长度不超过允许的最大历史长度
         current_context_size = len(next_context_tokens) + len(context_tokens)
         if current_context_size < max_history_length:
             context_tokens = next_context_tokens + context_tokens
@@ -78,31 +87,51 @@ def make_context(model, tokenizer,
 
 
 def parse_pot_no_stream(inputs):
+    """ 解析并处理输入字符串中特定格式（形如 <<...>>）的代码片段。
+        这些代码片段可以是简单的数学表达式赋值，也可以是定义和调用函数。
+        1. 对于包含 "func" 的代码片段，它会识别函数定义，执行该函数，
+           并将函数返回的结果替换到原始字符串中的相应位置。
+           如果函数涉及到 sympy（一个符号计算库），
+           则还会做一些特定的字符串替换处理。
+        2. 对于不包含 "func" 的代码片段，它会直接计算等号右边的表达式，
+           并将计算结果替换到原始字符串中，同时也会进行一些类型转换
+           （如将浮点数转为整数）。
+    """
     try:
+        # 尝试从输入字符串中找到形如 "<<...>>" 的模式
         s = re.findall(r'<<(.*?)>>', inputs, re.DOTALL)
+        # 如果没有找到匹配项，则直接返回原始输入
         if not s:
             #print("err inputs: ", origin_inputs, flush=True)
             return inputs
 
         index = 0
+        # 遍历所有匹配到的模式
         for k in s:
             try:
+                # 检查模式内是否包含 "func"
                 if "func" in k:
+                    # 分割并处理函数定义
                     var = k.split("=", 1)
                     try:
+                        # 去除空白字符并执行函数定义
                         var[1] = var[1].strip(" ")
                         exec(var[1], globals())
+                        # 调用函数获取结果
                         ans = func()
                     except:
+                        # 特殊处理包含 'sympy' 的情况
                         if 'sympy' in var[1]:
                             var[1] = var[1].replace('res[x]', 'res[0][0]').replace('res[y]', 'res[0][1]')
                             exec(var[1], globals())
                             ans = func()
                         pass
                     var_list = [c.strip(" ") for c in var[0].split(",")]
+                    # 如果只有一个变量名，则将结果放入列表
                     if len(var_list) == 1:
                         ans = [ans]
 
+                    # 将结果转换为浮点数或整数形式，并替换到输入字符串中
                     for i in range(len(ans)):
                         try:
                             ans[i] = float(ans[i])
@@ -111,22 +140,27 @@ def parse_pot_no_stream(inputs):
                         except:
                             pass
 
+                    # 替换原字符串中的模式和变量名
                     inputs = inputs.replace("<<"+k+">>", "")
                     for i in range(len(var_list)):
                         inputs = inputs.replace(var_list[i], str(ans[i]))
                     index += 1
+                    # 更新后续模式中的变量值
                     for c in range(index, len(s)):
                         for i in range(len(var_list)):
                             s[c] = s[c].replace(var_list[i], str(ans[i]))
                 else:
+                    # 处理非函数的情况，直接计算并替换
                     var = k.replace(" ", "").split("=")
                     var[1] = var[1].replace("eval", "")
                     ans = round(eval(var[1]), 10)
                     ans = float(ans)
                     if abs(ans - int(ans)) < 1e-10:
                         ans = str(int(ans))
+                    # 替换原字符串中的模式和变量名
                     inputs = inputs.replace("<<"+k+">>", "").replace(var[0], str(ans))
                     index += 1
+                    # 更新后续模式中的变量值
                     for c in range(index, len(s)):
                         s[c] = s[c].replace(var[0], str(ans))
             except:
@@ -138,16 +172,22 @@ def parse_pot_no_stream(inputs):
 
 
 class TextIterStreamer:
+    """ 实现文本的流式处理
+        能够逐个或逐段生成和输出文本，而不是一次性输出全部内容
+    """
     def __init__(self, tokenizer, skip_prompt=False, skip_special_tokens=False, use_pot=True):
         self.tokenizer = tokenizer
         self.skip_prompt = skip_prompt
         self.skip_special_tokens = skip_special_tokens
         self.tokens = []
+        # 使用队列来缓存生成的文本片段，以便于逐块输出
         self.text_queue = Queue()
         self.next_tokens_are_prompt = True
+        # 是否使用特定的后处理技术（例如翻译或优化），默认为True
         self.use_pot = use_pot
 
     def put(self, value):
+        # 接收并处理生成的token值
         if self.skip_prompt and self.next_tokens_are_prompt:
             self.next_tokens_are_prompt = False
         else:
@@ -166,6 +206,8 @@ class TextIterStreamer:
         return self
 
     def __next__(self):
+        # 实现迭代器的下一步方法，从队列中获取并返回文本，
+        # 或在无更多内容时抛出StopIteration异常
         value = self.text_queue.get()
         if value is None:
             raise StopIteration()
